cb4f077409cc5fd47cb0f326d760f4f757832ee0
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-04-18 16:57:03 +0900

    プレイヤーのマイグレーションファイルを追加

---
+++ migrations/sqls/20230418062900-create-players-table-up.sql
@@ -0,0 +1,9 @@
+/* Replace with your SQL commands */
+CREATE TABLE `players` (
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT "ユーザーID",
+  `name` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci' COMMENT "ユーザー名",
+  `password` VARCHAR(255) NOT NULL COLLATE 'utf8mb4_general_ci' COMMENT "パスワード",
+  `money` INT(11) UNSIGNED NOT NULL COMMENT "所持金",
+  `hp` INT(11) UNSIGNED NOT NULL COMMENT "体力",
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
