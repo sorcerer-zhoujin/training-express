f65334358338b34236f40d256a11d6d997147937
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-04-18 18:06:39 +0900

    プレイヤーテーブル制御を実装

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 import { HogeController } from "./hoge-controller";
 import { UserController } from "./user-controller";
+import { PlayerController } from "./player-controller";
 
-export { HogeController, UserController };
+export { HogeController, UserController, PlayerController };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import * as express from "express";
 import * as foo from "./foo";
 import * as users from "./users";
+import * as players from "./players";
 
 import { HogeController } from "../controllers";
 
@@ -15,3 +16,4 @@ router.get("/", (req, res, next) => {
 router.get("/errorSample", hogeController.errorResponse);
 router.use("/foo", foo.router);
 router.use("/users", users.router);
+router.use("/players", players.router);
