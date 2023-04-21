94dd4483a3423274b67d85c646be1496c00acf44
Zhou Jin <zhoujin@sorcerer.co.jp>
2023-04-21 17:22:05 +0900

    プレイヤーにアイテムを持たせる機能を追加

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,6 @@
 import { HogeController } from "./hoge-controller";
 import { UserController } from "./user-controller";
 import { PlayerController } from "./player-controller";
+import { PlayerItemController } from "./player-item-controller";
 
-export { HogeController, UserController, PlayerController };
+export { HogeController, UserController, PlayerController, PlayerItemController };
--- src/routes/players.ts
+++ src/routes/players.ts
@@ -1,11 +1,18 @@
 import * as express from "express";
 import { PlayerController } from "../controllers";
+import { PlayerItemController } from "../controllers";
 export const router = express.Router();
 
 const playerController = new PlayerController();
+const playerItemController = new PlayerItemController();
 
+// players
 router.get("/", playerController.getAllPlayers);
 router.get("/:playerId", playerController.getPlayerById);
 router.post("/", playerController.createPlayer);
 router.put("/:playerId", playerController.updatePlayer);
 router.delete("/:playerId", playerController.deletePlayer);
+
+//player_items
+router.get("/:playerId/getItems", playerItemController.getItems);
+router.post("/:playerId/addItem", playerItemController.addItem);
